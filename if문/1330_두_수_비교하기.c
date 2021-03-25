88bb7ab02587371f6f575d9c907af0e6b634c137
sieun960513 <sieun960513@naver.com>
2021-03-25 20:01:59 +0900

    \몰랑

--- if문/1330_두_수_비교하기.c
+++ if문/1330_두_수_비교하기.c
@@ -3,8 +3,17 @@ int main()
 {
     int A, B;
     scanf("%d %d", &A, &B);
-    if (A<B) {printf("<");}
-    else if(A>B) {printf(">");}
-    else {printf("==");}
+    if (A < B)
+    {
+        printf("<");
+    }
+    else if (A > B)
+    {
+        printf(">");
+    }
+    else
+    {
+        printf("==");
+    }
     return 0;
 }
--- if문/9498_시험성적.c
+++ if문/9498_시험성적.c
@@ -3,16 +3,25 @@ int main()
 {
     int A;
     scanf("%d", &A);
-    if (90<=A) {
+    if (90 <= A)
+    {
         printf("A");
     }
-    else if (80<=A) {
+    else if (80 <= A)
+    {
         printf("B");
-        }
-    else if (70<=A) {
+    }
+    else if (70 <= A)
+    {
         printf("C");
-        }
-    else if(60<=A){printf("D");}
-    else {printf("F");}
+    }
+    else if (60 <= A)
+    {
+        printf("D");
+    }
+    else
+    {
+        printf("F");
+    }
     return 0;
 }
