856bf03e51fc5132573f62a5eb806ad51ca84bf4
sieun960513 <sieun960513@naver.com>
2021-04-04 22:44:43 +0900

    무컴파일 방지 위원회

--- 1차원배열/2562_최댓값.c
+++ 1차원배열/2562_최댓값.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    printf("교수님 미워요");
+    int A = 0;
+    int index = 0;
+    int array[9] = {0};
+    for (int i = 0; i < 9; i++)
+    {
+        scanf("%d", &array[i]);
+        if (A < array[i])
+        {
+            A = array[i];
+            index = i + 1;
+        }
+    }
+    printf("%d\n%d", A, index);
     return 0;
 }
